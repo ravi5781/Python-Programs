99c85444d781698d495d8dca36cf46d106d1f1d4
ravi5781 <ravikiransh018@gmail.com>
2026-01-05 11:13:28 +0530

    prime number checking program

---
+++ Prime_number_check.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number: "))
+count=0
+
+if num>1:
+    for i in range(1, num+1):
+        if(num%i==0):
+            count+=1
+    if count==2:
+        print(num," is prime number")
+    else:
+        print(num," is not a prime number")
