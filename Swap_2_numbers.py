dae3914f530c0db3462ad606616d6e192813fe53
ravi5781 <ravikiransh018@gmail.com>
2026-01-05 11:06:56 +0530

    swapping 2 numbers

---
+++ Swap_2_numbers.py
@@ -0,0 +1,14 @@
+num1=int(input("enter number 1: "))
+num2=int(input("enter number 2: "))
+
+print("\nnum1 before swapping: ", num1)
+print("num2 before swapping:", num2)
+
+#approach1
+
+temp=num1
+num1=num2
+num2=temp
+
+print("\nnum1 after swapping: ", num1)
+print("num2 after swapping: ", num2)
